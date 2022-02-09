32b8ee5f3ed4ef91192eb842ff4cbb15dbc69a1a
Wesley <wesley@coinfo.com.br>
2022-02-09 09:39:05 -0300

    Inserindo operecoes assincronas Task, Async, Await

--- ProjectWebMvc/ProjectWebMvc/Services/DepartmentService.cs
+++ ProjectWebMvc/ProjectWebMvc/Services/DepartmentService.cs
@@ -1,4 +1,5 @@
-﻿using ProjectWebMvc.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using ProjectWebMvc.Data;
 using ProjectWebMvc.Models;
 using System;
 using System.Collections.Generic;
@@ -16,9 +17,9 @@ namespace ProjectWebMvc.Services
             _context = context;
         }
 
-        public List<Department> FindAll()
+        public async Task<List<Department>> FindAllAsync()
         {
-            return _context.Department.OrderBy(x => x.Name).ToList();
+            return await _context.Department.OrderBy(x => x.Name).ToListAsync();
         }
     }
 }
